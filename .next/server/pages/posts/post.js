837dd6b09273b1c855435fcd4fac801aaea383c1
Angela George <ageorge5@zagmail.gonzaga.edu>
2021-10-10 14:15:41 -0700

    Initial Log In Page

---
+++ .next/server/pages/posts/post.js
@@ -0,0 +1,46 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+(() => {
+var exports = {};
+exports.id = "pages/posts/post";
+exports.ids = ["pages/posts/post"];
+exports.modules = {
+
+/***/ "./pages/posts/post.js":
+/*!*****************************!*\
+  !*** ./pages/posts/post.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"default\": () => (/* binding */ Post)\n/* harmony export */ });\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react/jsx-dev-runtime */ \"react/jsx-dev-runtime\");\n/* harmony import */ var react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0___default = /*#__PURE__*/__webpack_require__.n(react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__);\nvar _jsxFileName = \"/Users/angelageorge/Desktop/Software Engineering/MyRead/pages/posts/post.js\";\n\nfunction Post() {\n  return /*#__PURE__*/(0,react_jsx_dev_runtime__WEBPACK_IMPORTED_MODULE_0__.jsxDEV)(\"h1\", {\n    children: \"Post Component\"\n  }, void 0, false, {\n    fileName: _jsxFileName,\n    lineNumber: 2,\n    columnNumber: 10\n  }, this);\n}//# sourceURL=[module]\n//# sourceMappingURL=data:application/json;charset=utf-8;base64,eyJ2ZXJzaW9uIjozLCJmaWxlIjoiLi9wYWdlcy9wb3N0cy9wb3N0LmpzLmpzIiwibWFwcGluZ3MiOiI7Ozs7Ozs7O0FBQWUsU0FBU0EsSUFBVCxHQUFnQjtBQUM3QixzQkFBTztBQUFBO0FBQUE7QUFBQTtBQUFBO0FBQUE7QUFBQSxVQUFQO0FBQ0QiLCJzb3VyY2VzIjpbIndlYnBhY2s6Ly9teS1yZWFkLy4vcGFnZXMvcG9zdHMvcG9zdC5qcz9jZjliIl0sInNvdXJjZXNDb250ZW50IjpbImV4cG9ydCBkZWZhdWx0IGZ1bmN0aW9uIFBvc3QoKSB7XG4gIHJldHVybiA8aDE+UG9zdCBDb21wb25lbnQ8L2gxPlxufSJdLCJuYW1lcyI6WyJQb3N0Il0sInNvdXJjZVJvb3QiOiIifQ==\n//# sourceURL=webpack-internal:///./pages/posts/post.js\n");
+
+/***/ }),
+
+/***/ "react/jsx-dev-runtime":
+/*!****************************************!*\
+  !*** external "react/jsx-dev-runtime" ***!
+  \****************************************/
+/***/ ((module) => {
+
+module.exports = require("react/jsx-dev-runtime");
+
+/***/ })
+
+};
+;
+
+// load runtime
+var __webpack_require__ = require("../../webpack-runtime.js");
+__webpack_require__.C(exports);
+var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
+var __webpack_exports__ = (__webpack_exec__("./pages/posts/post.js"));
+module.exports = __webpack_exports__;
+
+})();
---
+++ pages/_authenticationModal.js
@@ -0,0 +1,55 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Modal, Button, Form} from 'react-bootstrap';
+import React, { useState } from 'react';
+
+function AuthenticationModal({show, handleClose, type}) {
+    function handleSubmit(e){
+        console.log(email)
+        console.log(password)
+    }
+
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    
+    return (
+        <Modal
+        size="lg"
+        aria-labelledby="contained-modal-title-vcenter"
+        show={show} 
+        onHide={handleClose}
+        centered
+        >
+        <Modal.Header closeButton>
+            <Modal.Title id="contained-modal-title-vcenter">
+           {type}
+            </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+            <Form>
+                <Form.Group className="mb-3" controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control value={email} type="email" placeholder="Enter email" onChange={(e) => setEmail(e.currentTarget.value)} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control value={password} type="password" placeholder="Password" onChange={(e) => setPassword(e.currentTarget.value)} />
+                </Form.Group>
+
+                <Form.Group className="mb-3" controlId="formBasicCheckbox">
+                    {type == "Register"? <Form.Check type="checkbox" label="I accept the terms and conditions"/> : <></> }
+                </Form.Group>
+
+                <Button variant="primary" type="submit" onClick={(e)=>handleSubmit(e.currentTarget)}>
+                    Submit
+                </Button>
+                </Form>
+        </Modal.Body>
+        <Modal.Footer>
+            <Button onClick={handleClose}>Close</Button>
+        </Modal.Footer>
+        </Modal>
+    );
+}
+
+export default AuthenticationModal
