fe3b4176c4fcee3fb11fa11e4fc24f882a0d11fc
Sam Toll <sam.x.toll@gmail.com>
2021-11-01 18:02:44 -0700

    server addition

---
+++ Server/models/User.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose');
+
+const UserSchema = new mongoose.Schema({
+    firstName: {
+        type: String,
+        required: true,
+    },
+    lastName: {
+        type: String,
+        required: true,
+    },
+    alias: {
+        type: String,
+        required: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: true,
+    },
+    phoneNumber: {
+        type: String,
+        required: true,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    },
+    picture: {
+         type: String,
+    },
+  });
+  
+  module.exports = User = mongoose.model('user', UserSchema);
+  
---
+++ Server/routes/api/users.js
@@ -0,0 +1,84 @@
+const express = require('express');
+const router = express.Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
+
+const User = require('../../models/User');
+
+// @route   GET api/users
+// @desc    Register user
+// @access  Public
+router.post(
+  '/',
+  [
+    check('firstName', 'First Name is required').not().isEmpty(),
+    check('lastName', 'Last Name is required').not().isEmpty(),
+    check('alias', 'Alias is required').not().isEmpty(),
+    check('email', 'Please include a valid email').isEmail(),
+    check(
+      'password',
+      'Please enter a password with 6 or more characters'
+    ).isLength({ min: 6 }),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { firstName, lastName, email, alias, password, phoneNumber } = req.body;
+
+    try {
+      // See if the user exists
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      user = new User({
+        firstName,
+        lastName,
+        email,
+        alias,
+        password,
+        phoneNumber,
+      });
+
+      // Encrypt password
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      // Place await in front of anything that returns a promise
+      await user.save();
+
+      // Return jsonWebToken
+      const payload = {
+        user: {
+          id: user._id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server Error');
+    }
+
+    console.log(req.body);
+  }
+);
+
+module.exports = router;
