5b590ae04d5b595fbcbb67be04d1c303710685bc
Angela George <ageorge5@zagmail.gonzaga.edu>
2021-11-10 14:34:03 -0800

    heroku env??

--- Server/config/db.js
+++ Server/config/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 const config = require('config');
-const db = process.env.mongoURI;
+const db = process.env.MONGODB_URI;
 
 // We are using async await...
 const connectDB = async () => {
