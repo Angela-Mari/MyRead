88d0892b742a660a7f823bb46e261a31fe256a79
Cole Thorpen <thorpencole@gmail.com>
2021-11-16 20:30:35 -0800

    bad request w/ Google. need to get pword and phone

--- Client/src/App.js
+++ Client/src/App.js
@@ -54,7 +54,7 @@ function App({
   } 
 
   function handleGoogleSubmit(g) {
-    setShow(false);
+    //setShow(false);
     console.log('inside handleGoogleSubmit');
     console.log("in app: ", g);
     setFirstName(g.getGivenName());
@@ -62,7 +62,7 @@ function App({
     setEmail(g.getEmail());
     setAlias(g.getEmail().split("@")[0].toLowerCase());
 
-    handleSubmit(g); //need password and phone before signing up. save them and use when using google
+    //handleSubmit(g); //need password and phone before signing up. save them and use when using google
 
     // setTwoFA(true);
   }
