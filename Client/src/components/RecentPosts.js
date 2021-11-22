b86a2c17dc0c5de74867d1737dddf17931465334
Angela George <ageorge5@zagmail.gonzaga.edu>
2021-11-22 17:58:38 -0600

    async?

--- Client/src/components/RecentPosts.js
+++ Client/src/components/RecentPosts.js
@@ -6,19 +6,17 @@ import { connect } from 'react-redux';
 import PropTypes from 'prop-types';
 
 function RecentPosts({getPosts}) {
-    var backendPosts;
+    var postsArray;
     async function backendPosts(){
         backendPosts = await getPosts();
         console.log(backendPosts) // this is getting a return!
+        postsArray = backendPosts.data.map((postIndex, index) => {
+            return (<Post title = {postIndex['title']} text = {postIndex['description']} link = {postIndex['url']} key = {index} id={postIndex['_id']}> </Post>);})
+        
     }
     
     useEffect(()=>backendPosts())
 
-    const posts = backendPosts.data
-
-    const postsArray = posts.map((postIndex, index) => {
-        return (<Post title = {postIndex['title']} text = {postIndex['description']} link = {postIndex['url']} key = {index} id={postIndex['_id']}> </Post>);})
-    
     return (
     <Col>
         <h2>Recent Posts</h2>
