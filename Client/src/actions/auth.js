30a31291e48fe8acab11246672ff5917000fab8b
Sam Toll <sam.x.toll@gmail.com>
2021-11-22 18:08:14 -0800

    server bug-fix

--- Client/src/actions/auth.js
+++ Client/src/actions/auth.js
@@ -13,21 +13,26 @@ import {
 } from './types';
 import setAuthToken from '../utils/setAuthToken';
 
-var isDev = process.env.NODE_ENV == null; 
-var devPrefix = "";
+function getDevPrefix() { 
+  var devPrefix = "http://localhost:5000";
 
-if(isDev) {
-  devPrefix = "http://localhost:5000";
-} else {
-  devPrefix = "";
+  if(process === undefined) {
+    console.log(">>> Process doesnt exist, dev mode activated");
+    return devPrefix;
+  } else if(process.env.NODE_ENV == null) {
+    console.log(">>> NODE_ENV is null, dev mode activated");
+    return devPrefix;
+  } else if(process.env.NODE_ENV === undefined) {
+    console.log(">>> NODE_ENV undefined, dev mode activated");
+    return devPrefix;
+  } else if(process.env.NODE_ENV === "development") {
+    console.log(">>> NODE_ENV is set to development, dev mode activated");
+    return devPrefix;    
+  } else {
+    console.log(">>> No dev mode detected");
+    return "";
+  }
 }
-
-const api = axios.create({
-  baseURL: 'http://localhost:5000/api',
-  headers: {
-    'Content-Type': 'application/json',
-  },
-});
 
 // Load User
 export const loadUser = () => async (dispatch) => {
@@ -36,7 +41,7 @@ export const loadUser = () => async (dispatch) => {
     }
   
     try {
-      const res = await axios.get(devPrefix + '/api/auth');
+      const res = await axios.get(getDevPrefix() + '/api/auth');
   
       dispatch({
         type: USER_LOADED,
@@ -66,7 +71,7 @@ export const register = (
     var body = JSON.stringify({ firstName, lastName, alias, email, password, phoneNumber });
   
     try {
-      const res = await axios.post(devPrefix + '/api/users', body, config);
+      const res = await axios.post(getDevPrefix() + '/api/users', body, config);
   
       dispatch({
         type: REGISTER_SUCCESS,
@@ -100,7 +105,7 @@ export const login = (email, password) => async (dispatch) => {
     };
   
     try {
-      const res = await axios.post(devPrefix + '/api/auth', body, config);
+      const res = await axios.post(getDevPrefix() + '/api/auth', body, config);
       // const res = await axios.post('/api/auth', body);
       console.log(res);
       dispatch({
