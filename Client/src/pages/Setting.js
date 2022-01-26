cb11e966113e8861659871b6437ac3cc756325e5
htang97 <htang2@zagmail.gonzaga.edu>
2022-01-26 15:39:01 -0800

    updates

--- Client/src/pages/Setting.js
+++ Client/src/pages/Setting.js
@@ -1,32 +1,69 @@
-import React from "react";
+import React, { useState } from "react";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 import { Row, Button, Container } from "react-bootstrap";
 import "../assets/css/setting.css";
-import "./Settings.css"
+import "./Settings.css";
 function Setting() {
+    const [isshowpay, setisshowpay] = useState(false);
+    const paypalparams = {
+        /*  Please enter your own clent-ID   
+            Create your app in the paypal console and go to the client-ID  choose live create app
+            https://developer.paypal.com/developer/applications
+        */
+        // xxxxxxxxxxxxxxe
+        "client-id": "AYTcIpoc8y_ScX8g2EhiQ-tgrDgrbKj3C6a9dHpeGL3cqy5kjHq-e3UBGb2Cq2E47B_QT-P30yivl1Tm",
+    };
+    const createOrder = (data, actions) => {
+        return actions.order.create({
+            purchase_units: [
+                {
+                    amount: {
+                        value: "0.01",
+                    },
+                },
+            ],
+        });
+    };
+    const onApprove = (data, actions) => {
+        return actions.order.capture();
+    };
+    const payshow = () => {
+        setisshowpay(true);
+    };
+
     return (
         <Container fluid={true}>
-                <Row>
-                    <h2 className="my-title">Settings</h2>
-                </Row>
-                <Row className="line">
-                    <h2>Privacy</h2>
-                    <p>Your profile is public, toggle to change.</p>
-                    <p>
-                        <div class="form-check form-switch">
-                            <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
-                        </div>
-                    </p>
-                </Row>
-                <Row className="line">
-                    <h2>Payment</h2>
-                    <p>Your are on the free tier.</p>
-                    <Button variant="primary">Upgrade to Premium</Button>
-                </Row>
-                <Row className="line">
-                    <h2>Delete Account</h2>
-                    <p>Permanatley delete account and lose all saved posts?</p>
-                    <Button variant="primary">Delete Account</Button>
-                </Row>
+            <Row>
+                <h2 className="my-title">Settings</h2>
+            </Row>
+            <Row className="line">
+                <h2>Privacy</h2>
+                <p>Your profile is public, toggle to change.</p>
+                <p>
+                    <div class="form-check form-switch">
+                        <input class="form-check-input" type="checkbox" role="switch" id="flexSwitchCheckDefault" />
+                    </div>
+                </p>
+            </Row>
+            <Row className="line">
+                <h2>Payment</h2>
+                <p>Your are on the free tier.</p>
+                <Button variant="primary" onClick={payshow}>
+                    Upgrade to Premium
+                </Button>
+                {isshowpay && (
+                    <Button variant="primary" className="paybut">
+                        <PayPalScriptProvider options={paypalparams}>
+                            <PayPalButtons createOrder={createOrder} onApprove={onApprove} />
+                        </PayPalScriptProvider>
+                    </Button>
+                )}
+            </Row>
+            <Row className="line">
+                <h2>Delete Account</h2>
+                <p>Permanatley delete account and lose all saved posts?</p>
+                <Button variant="primary">Delete Account</Button>
+            </Row>
         </Container>
     );
 }
